5309d371be29e3969c901d86f05deae1c7542cca
hkgnp <ben@pngs.cc>
2021-05-18 00:13:39 +0800

    -

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
 const mongoUrl = process.env.MONGO_URL;
 const MongoUtil = require('./MongoUtil');
-const ObjectId = require('mongodb').ObjectId;
+// const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 
@@ -11,10 +12,10 @@ app.use(express.json());
 app.use(cors());
 
 (async () => {
-  const db = await MongoUtil.connect(mongoUrl, process.env.DBNAME);
+  const db = await MongoUtil.connect(mongoUrl, 'carecosts-sg');
 
   app.get('/', async (req, res) => {
-    let result = await db.collection('baskets').find({}).toJSON();
+    let result = await db.collection('baskets').find({}).toArray();
     res.status(200);
     res.send(result);
   });
